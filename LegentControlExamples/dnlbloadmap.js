77a74f8a4710071a61013b89d0b848b44486c8b4
Sandeep Sharma <46123034+sssandeepsharma46@users.noreply.github.com>
2023-12-20 03:37:11 +0530

    legend control file

--- LegentControlExamples/dnlbloadmap.js
+++ LegentControlExamples/dnlbloadmap.js
@@ -6,6 +6,7 @@ var dnlbMap = (function(){
     }
     
     var map;
+    var legend;
     var subsKey = "Ju0RuuORzuihf0yrr1e2_XVmciuMrSuu-EVovuptO5g";
     var defaultMapSetting = {
         language: "en-US",
@@ -13,12 +14,18 @@ var dnlbMap = (function(){
         zoom: 10,
         view: "Auto"
     };
-    var legend;
-    var legendFullnessColor = {
-        "0%-50%": { Name: "0%-50%", Color: "rgb(123, 188, 78)" },
-        "51%-75%": { Name: "51%-75%", Color: "rgb(250, 171, 52)" },
-        "76%-100%": { Name: "76%-100%", Color: "rgb(237, 77, 30)" }
-      };
+    
+    var cateogryLegend = {
+        "circle" : {shape: "circle", label: "Circle1", color: "red"},
+        "square": {shape: "square", label: "Square1", color: "green"},
+        "triangle": {shape: "triangle", label: "triangle1", color: "blue"}
+    }
+    var gradientLegend = {
+        "low" : {offset: 0, label: "low", color: "green"},
+        "medium": {offset: 0.5, label: "medium", color: "yellow"},
+        "high": {offset: 1, label: "high", color: "red"}
+    }
+    
     var loadMap = function(){
         map = new atlas.Map("myMap",{
             language: defaultMapSetting.language,
@@ -31,84 +38,60 @@ var dnlbMap = (function(){
                 "subscriptionKey": subsKey
             }
         });
-        addLegentControl();
+        addLegendControl();
     };
     
-    var addLegentControl = function(){
-        
+    var addLegendControl = function(){
         legend = new atlas.control.LegendControl({
-      //Global title to display for the legend.
-      title: "Legend",
-      //Hide the button to collapse the legend.
-      showToggle: true,
-      //All legend cards to display within the legend control.
-      legends: [
-        {
-                            type: 'category',
-                            subtitle: 'Category',
-                            layout: 'column',
-                            itemLayout: 'row',
-                            footer: 'A category legend that uses a combination of shapes and icons.',
-                            strokeWidth: 2,
-                            items: [
-                                {
-                                    color: 'DodgerBlue',
-                                    label: 'Hello',
-                                    shape: 'https://azuremapscodesamples.azurewebsites.net/Common/images/icons/campfire.png'
-                                }, {
-                                    color: 'Yellow',
-                                    label: 'World',
-                                    shape: 'square'
-                                }, {
-                                    color: 'Orange',
-                                    label: 'Ricky',
-                                    shape: 'line'
-                                }, {
-                                    color: 'Red',
-                                    label: 'is',
-                                    shape: 'circle'
-                                }, {
-                                    color: 'purple',
-                                    label: 'awesome!',
-                                    shape: 'triangle'
-                                }
-                            ]
+            title: "This is legend title",
+            legends:[
+                {
+                    type: "category",
+                    subtitle: "this is category subtitle",
+                    layout: "column",
+                    itemLayout: "row",
+                    footer: "This is footer message",
+                    items:[
+                        {
+                            label: cateogryLegend.circle.label,
+                            color: cateogryLegend.circle.color,
+                            shape: "/WebResources/dnlb_/images/clickme.jpg"
                         },{
-                            type: 'gradient',
-                            subtitle: 'Gradient legend',
-                            footer: 'A gradient legend that uses multiple color stops, some having labels.',
-
-                            stops: [
-                                {
-                                    offset: 0,
-                                    color: 'royalblue',
-                                    label: 'low'
-                                },
-                                {
-                                    offset: 0.25,
-                                    color: 'cyan'
-                                },
-                                {
-                                    offset: 0.5,
-                                    color: 'lime',
-                                    label: 'medium'
-                                }, 
-                                {
-                                    offset: 0.75,
-                                    color: 'yellow'
-                                },{
-                                    offset: 1,
-                                    color: 'red',
-                                    label: 'high'
-                                }
-                            ]
+                            label: cateogryLegend.square.label,
+                            color: cateogryLegend.square.color,
+                            shape: cateogryLegend.square.shape
+                        },{
+                            label: cateogryLegend.triangle.label,
+                            color: cateogryLegend.triangle.color,
+                            shape: cateogryLegend.triangle.shape
                         }
-      ]
-    });
-        //Add the legend control to the map.
-        map.controls.add(legend, {
-          position: "bottom-left",
+                    ]
+                },{
+                    type: "gradient",
+                    subtitle: "This is gradient subtitle",
+                    layout: "column",
+                    footer: "This is gradient footer messate",
+                    stops:[
+                        {
+                            offset: gradientLegend.low.offset,
+                            color: gradientLegend.low.color,
+                            label: gradientLegend.low.label
+                        },
+                        {
+                            offset: gradientLegend.medium.offset,
+                            color: gradientLegend.medium.color,
+                            label: gradientLegend.medium.label
+                        },
+                        {
+                            offset: gradientLegend.high.offset,
+                            color: gradientLegend.high.color,
+                            label: gradientLegend.high.label
+                        }
+                    ]
+                }
+            ]
         });
+        map.controls.add(legend,{position: "top-left"});
     }
     
     return{
